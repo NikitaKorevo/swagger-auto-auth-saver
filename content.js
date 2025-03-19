5f3291f898fcdd093c35aa52f0bfe9802d1ceac0
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-03-19 17:51:00 +0300

    feat: save or remove localStorage value by mouseup event

--- content.js
+++ content.js
@@ -1,26 +1,48 @@
+const localStorageKey = 'SwaggerAutoAuthSaverValue';
 const hostname = location.hostname;
 const id = hostname === 'editor.swagger.io' ? 'swagger-editor' : 'swagger-ui';
 const swaggerElement = document.getElementById(id);
 
-const main = async authWrapperElement => {
-  let form = authWrapperElement.querySelector('form');
-  let input = form?.querySelector('input');
-  let buttons = form?.querySelectorAll('button');
+const updateLocalStorageValue = authWrapperElement => {
+  const form = authWrapperElement.querySelector('form');
+  const buttons = form?.querySelectorAll('button');
 
-  let changeEvent = new Event('change', { bubbles: true });
+  if (!buttons || !form) {
+    return;
+  }
+
+  const [authButtonElement] = buttons;
+
+  authButtonElement?.addEventListener('mouseup', () => {
+    const input = form.querySelector('input');
+
+    if (input) {
+      localStorage.setItem(localStorageKey, input.value);
+    } else {
+      localStorage.removeItem(localStorageKey);
+    }
+  });
+};
+
+const setInitialValue = authWrapperElement => {
+  const form = authWrapperElement.querySelector('form');
+  const input = form?.querySelector('input');
+  const buttons = form?.querySelectorAll('button');
+
+  const changeEvent = new Event('change', { bubbles: true });
 
   if (input) {
-    const value = localStorage.getItem('SwaggerAutoAuthSaverValue') ?? '';
+    const value = localStorage.getItem(localStorageKey) ?? '';
     input.value = value;
+
     input.dispatchEvent(changeEvent);
   }
 
   if (buttons) {
     const [authButtonElement, closeButtonElement] = buttons;
-    authButtonElement.click();
-    closeButtonElement.click();
 
-    closeButtonElement.addEventListener('click', () => console.log('click'));
+    authButtonElement?.click();
+    closeButtonElement?.click();
   }
 };
 
@@ -29,12 +51,18 @@ const openAuthModal = authWrapperElement => {
     return;
   }
 
+  let isFirstOpened = true;
+
   const mutationObserver = new MutationObserver(() => {
-    main(authWrapperElement);
-    mutationObserver.disconnect();
+    if (isFirstOpened) {
+      setInitialValue(authWrapperElement);
+      isFirstOpened = false;
+    } else {
+      updateLocalStorageValue(authWrapperElement);
+    }
   });
 
-  mutationObserver.observe(swaggerElement, { childList: true, subtree: true });
+  mutationObserver.observe(authWrapperElement, { childList: true });
   authWrapperElement.querySelector('button').click();
 };
 
